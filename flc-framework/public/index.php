00a57559663df326f2752d524f8545de0f3c85fc
xhj8816216 <xhj8816216@163.com>
2017-08-06 11:10:36 +0800

    wordpress接口

--- flc-framework/public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// 入口文件
-require_once __DIR__ . '/../bootstrap.php';
-
-// 路由配置
-require_once __DIR__ . '/../config/routes.php';
--- thinkphp-api/Application/Home/Conf/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-    //加载配置文件
-    'LOAD_EXT_CONFIG' => 'app',
-);
